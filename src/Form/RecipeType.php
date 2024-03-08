05bb7d23845b51e3a3abfd5e0cbd5b5fbaf45c8e
Jayce <jcmestas@gmail.com>
2024-03-08 21:47:07 +0100

    ORM, Relation Recipe Category ManyToOne

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -2,19 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Recipe;
-use DateTimeImmutable;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Event\PostSubmitEvent;
-use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Validator\Constraints\Sequentially;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -35,10 +30,11 @@ class RecipeType extends AbstractType
             ])
             ->add('slug', TextType::class, [
                 'required' => false,
-                'constraints' => new Sequentially([
-                    new Length(min: 10),
-                    new Regex('/^[a-z0-9]+(?:-[a-z0-9]+)*$/', message: "Ceci n'est pas un slug valide")
-                ])
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'expanded' => true,
             ])
             ->add('content', TextareaType::class, [
                 'empty_data' => ''
