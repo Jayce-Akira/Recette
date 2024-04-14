1e92da7801bb6c2c72ef82ca6e7b8fcda64ad5d4
Jayce <jcmestas@gmail.com>
2024-04-14 15:51:59 +0200

    serializer

---
+++ src/Controller/API/RecipesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class RecipesController extends AbstractController
+{
+    #[Route("/api/recipes")]
+    public function index(RecipeRepository $repository, Request $request, SerializerInterface $serializer)
+    {
+        $recipes = $repository->paginateRecipe($request->query->getInt('page', 1));
+        // dd($serializer->serialize($recipes, 'csv', [
+        //     'groups' => ['recipes.index']
+        // ]));
+        return $this->json($recipes, 200, [], [
+            'groups' => ['recipes.index']
+        ]);
+    }
+
+    #[Route("/api/recipes/{id}", requirements: ['id' => Requirement::DIGITS])]
+    public function show(Recipe $recipe)
+    {
+        return $this->json($recipe, 200, [], [
+            'groups' => ['recipes.index', 'recipes.show']
+        ]);
+    }
+}
